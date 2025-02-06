1e61a3affe00dcac33876873489c596c7dc669bf
heauchi <tesupark00@gmail.com>
2025-02-06 15:46:01 +0900

    feat: 게시물 보이기 메인화면 구현. testapi사용.

---
+++ FE/lib/screens/home_screen.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'post_screen.dart';
+import 'post.dart'; // Post 모델 분리
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final PageController _pageController = PageController();
+  List<Post> _posts = [];
+  int _currentIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchPosts(); // 첫 번째 게시물 로드
+  }
+
+  // API에서 게시물 가져오기
+  Future<void> _fetchPosts() async {
+    final url = Uri.parse(
+        'https://v79h9dyx08.execute-api.ap-northeast-2.amazonaws.com/WeaveAPI/GetPostList');
+
+    final body = jsonEncode({
+      'user_id': '1',
+      'post_type': '12345678',
+      'username': 'test_account',
+      'password': '12345678'
+    });
+
+    final headers = {
+      'Content-Type': 'application/json; charset=utf-8',
+      'x-api-key': '4ckQxZOZt68Xa7kYVoAg43vsy3W5f2631KjqYMpP',
+    };
+
+    try {
+      final response = await http.post(url, headers: headers, body: body);
+
+      if (response.statusCode == 200) {
+        final decodedBody = utf8.decode(response.bodyBytes);
+        final data = jsonDecode(decodedBody);
+        List<Post> newPosts = [];
+
+        for (var item in data['results']) {
+          newPosts.add(Post.fromJson(item));
+        }
+
+        setState(() {
+          _posts.addAll(newPosts);
+        });
+      } else {
+        print('Error: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('API 요청 실패: $e');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView.builder(
+        controller: _pageController,
+        itemCount: _posts.length + 1, // 다음 요청 공간 포함
+        onPageChanged: (index) {
+          if (index == _posts.length) {
+            _fetchPosts(); // 마지막 페이지에서 추가 요청
+          }
+          setState(() => _currentIndex = index);
+        },
+        itemBuilder: (context, index) {
+          if (index < _posts.length) {
+            return PostScreen(postData: _posts[index]); // PostScreen에 데이터 전달
+          } else {
+            return Center(child: CircularProgressIndicator()); // 로딩 인디케이터
+          }
+        },
+      ),
+    );
+  }
+}
---
+++ FE/lib/screens/post_screen.dart
@@ -0,0 +1,34 @@
+import 'dart:convert'; // JSON 변환에 필요
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'post.dart';
+
+class PostScreen extends StatefulWidget {
+  final Post postData; // Post 데이터 받기
+
+  const PostScreen({Key? key, required this.postData}) : super(key: key);
+
+  @override
+  _PostScreenState createState() => _PostScreenState();
+}
+
+class _PostScreenState extends State<PostScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(widget.postData.weaveTitle)), // 제목 표시
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(widget.postData.content, style: TextStyle(fontSize: 16)),
+            SizedBox(height: 10),
+            if (widget.postData.urls.isNotEmpty)
+              Image.network(widget.postData.urls.first), // 첫 번째 이미지 표시
+          ],
+        ),
+      ),
+    );
+  }
+}
