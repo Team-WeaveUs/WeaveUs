0e3b22e946614ae64335ea638bf4ef3b0170bea0
heauchi <tesupark00@gmail.com>
2025-02-08 18:45:28 +0900

    feat : make profile_screen.dart, and toggle_button.dart featured toggle screen what follower_tab.dart and profile_tab.dart

---
+++ FE/lib/screens/profile_screen/profile_tab/post_grid.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class PostGrid extends StatelessWidget {
+  const PostGrid({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("게시물 목록");
+  }
+}
---
+++ FE/lib/screens/profile_screen/profile_tab/reward_list.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class RewardList extends StatelessWidget {
+  const RewardList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("리워드 리스트");
+  }
+}
