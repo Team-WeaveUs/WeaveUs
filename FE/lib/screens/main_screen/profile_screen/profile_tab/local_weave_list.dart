2f639566b01030e9b5fb17ce2b491e2165145e00
heauchi <tesupark00@gmail.com>
2025-02-17 00:23:33 +0900

    feat : reward_list.dart 삭제, 로컬위브 공간 생성

---
+++ FE/lib/screens/main_screen/profile_screen/profile_tab/local_weave_list.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class LocalWeaveList extends StatelessWidget {
+  const LocalWeaveList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("비밀 위브 리스트 들어갈 곳"),
+    );
+  }
+}
