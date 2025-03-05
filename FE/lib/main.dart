bbab67dda04a775c41fc52093b38d376ccbe3402
박태수 <tesupark00@gmail.com>
2025-03-05 11:01:10 +0900

    feat : web일때는 env파일 사용 안함.

--- FE/lib/main.dart
+++ FE/lib/main.dart
@@ -1,9 +1,10 @@
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:weave_us/screens/signin_screen.dart';
 import 'package:weave_us/screens/main_screen.dart';
 import 'package:weave_us/screens/owner_screen.dart';
-
+import "config.dart";
 // fe/dev(아무개) Branch 생성
 // fe/dev 에서 작업 및 커밋
 // fe/dev 를 fe/main 으로 pr
@@ -11,7 +12,7 @@ import 'package:weave_us/screens/owner_screen.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load(fileName: ".env");
+  if (!kIsWeb) {await dotenv.load();}
   runApp(const MyApp());
 }
 
@@ -23,8 +24,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false, // 배너 제거
-      //home: SigninScreen(), // SigninScreen 먼저 보여주기,
-      home: MainScreen(), //Sign in api 안돼서 main먼저 보여주기
+      home: SigninScreen(), // SigninScreen 먼저 보여주기,
+      //home: MainScreen(), //Sign in api 안돼서 main먼저 보여주기
     );
   }
 }
