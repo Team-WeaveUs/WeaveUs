b50902329fb2137bb012ff49bd0e223f44780ffe
heauchi <tesupark00@gmail.com>
2025-03-06 18:58:11 +0900

    feat: get요청도 추가

--- FE/lib/Auth/api_client.dart
+++ FE/lib/Auth/api_client.dart
@@ -54,4 +54,35 @@ class ApiService {
     }
     return false;
   }
+  static Future<dynamic> sendGetRequest(String endpoint) async {
+    String? accessToken = await TokenStorage.getAccessToken();
+
+    try {
+      final response = await http.get(
+        Uri.parse(
+            "https://v79h9dyx08.execute-api.ap-northeast-2.amazonaws.com/$endpoint"),
+        headers: {
+          'Content-Type': 'application/json',
+          'accesstoken': '$accessToken',
+        },
+      );
+
+      if (response.statusCode == 200) {
+        return jsonDecode(response.body);
+      } else if (response.statusCode == 401) {
+        // 액세스 토큰 만료 -> 토큰 재발급 시도
+        bool refreshed = await _refreshToken();
+        if (refreshed) {
+          return sendGetRequest(endpoint);
+        } else {
+          return null;
+        }
+      } else if (response.statusCode == 403 || response.statusCode == 500) {
+        return null;
+      }
+    } catch(e){
+      print('Error fetching profile: $e');
+      return null;
+    }
+  }
 }
