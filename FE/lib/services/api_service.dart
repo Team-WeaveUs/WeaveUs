b8f74defff04c487ebf0ef2e072eaf768426a1a5
박태수 <tesupark00@gmail.com>
2025-03-31 23:51:46 +0900

    feat: Update base URL and add API path for API calls
    - Updated the base URL to `https://v79h9dyx08.execute-api.ap-northeast-2.amazonaws.com`.
    - Added an `apiPath` constant `/WeaveAPI`.
    - Implemented `getApiUrl` function to construct the full API URL.
    - Implemented a `get` function to handle GET requests.

--- FE/lib/services/api_service.dart
+++ FE/lib/services/api_service.dart
@@ -1,5 +1,22 @@
 import 'package:http/http.dart' as http;
 
 class ApiService {
-  static const String baseUrl = 'https://api.example.com';
+  static const String baseUrl = 'https://v79h9dyx08.execute-api.ap-northeast-2.amazonaws.com';
+  static const String apiPath = '/WeaveAPI';
+
+  static String getApiUrl(String path) {
+    return '$baseUrl$apiPath$path';
+  }
+
+  static Future<http.Response> get(String path) async {
+    final url = getApiUrl(path);
+    final response = await http.get(
+        Uri.parse(url),
+        headers: {
+          'Content-Type': 'application/json; charset=UTF-8',
+          //'accesstoken': accessToken, accesstoken 받아오는 tokenservice가 필요함.
+        }
+    );
+    return response;
+  }
 }
