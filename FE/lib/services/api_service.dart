bf3c07c332a1f53eed110a6ac1693204fe467a0d
heauchi <tesupark00@gmail.com>
2025-04-02 15:55:51 +0900

    feat: API 요청 기능 개선 및 토큰 기반 인증 적용
    -   `ApiService`를 개선하여 GET 및 POST 요청을 처리하도록 변경.
    -   `TokenService`를 사용하여 요청에 토큰을 포함.
    -   401 응답을 처리하고, 토큰을 갱신한 후 재요청을 수행하는 로직 추가.
    - `AuthController`를 활용하여 토큰 갱신 핸들링을 하도록 구현.

--- FE/lib/services/api_service.dart
+++ FE/lib/services/api_service.dart
@@ -1,22 +1,53 @@
+import 'dart:convert';
+import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
+import 'package:weave_us/models/lambda_response_model.dart';
+import 'package:weave_us/services/token_service.dart';
+import '../controllers/auth_controller.dart';
 
 class ApiService {
-  static const String baseUrl = 'https://v79h9dyx08.execute-api.ap-northeast-2.amazonaws.com';
-  static const String apiPath = '/WeaveAPI';
+  final AuthController _authController = Get.find<AuthController>();
+  final TokenService _tokenService = Get.find<TokenService>();
 
-  static String getApiUrl(String path) {
-    return '$baseUrl$apiPath$path';
+  Future<Map<String, dynamic>> getRequest(String endpoint) async {
+    final token = await _tokenService.loadToken();
+    if (token == null) throw Exception("No token found");
+    final url = "https://v79h9dyx08.execute-api.ap-northeast-2.amazonaws.com/WeaveAPI/$endpoint";
+
+    final lambdaResponse = await http.get(
+      Uri.parse(url),
+      headers: {'accesstoken': token.accessToken},
+    );
+
+    final LambdaResponse response = LambdaResponse.fromJson(jsonDecode(lambdaResponse.body));
+
+    if (response.statusCode == 401) {
+      bool refreshed = await _authController.handle401();
+      if (refreshed) {
+        return getRequest(url); // 다시 요청
+      }
+    }
+    return response.body;
   }
 
-  static Future<http.Response> get(String path) async {
-    final url = getApiUrl(path);
-    final response = await http.get(
-        Uri.parse(url),
-        headers: {
-          'Content-Type': 'application/json; charset=UTF-8',
-          //'accesstoken': accessToken, accesstoken 받아오는 tokenservice가 필요함.
-        }
+  Future<Map<String, dynamic>> postRequest(String endpoint, Map<String, dynamic> body) async {
+    final token = await _tokenService.loadToken();
+    if (token == null) throw Exception("No token found");
+    final url = "https://v79h9dyx08.execute-api.ap-northeast-2.amazonaws.com/WeaveAPI/$endpoint";
+
+    final lambdaResponse = await http.post(
+      Uri.parse(url),
+      headers: {'accesstoken': token.accessToken},
+      body: jsonEncode(body),
     );
-    return response;
+
+    final LambdaResponse response = LambdaResponse.fromJson(jsonDecode(lambdaResponse.body));
+    if (response.statusCode == 401) {
+      bool refreshed = await _authController.handle401();
+      if (refreshed) {
+        return postRequest(url, body); // 다시 요청
+      }
+    }
+    return response.body;
   }
 }
