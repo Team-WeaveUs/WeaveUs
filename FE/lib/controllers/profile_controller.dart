621caadd2462d3dd6a1f363b5db51e9bbb4b67cd
heauchi <68429278+tesupark@users.noreply.github.com>
2025-04-10 17:10:28 +0900

    feat: 프로필 정보 조회 기능 구현
    - 프로필 정보 조회를 위한 `ProfileController`를 구현
    - `_fetchMyProfile()`을 통해 내 프로필 정보 조회
    - `fetchProfile()`을 통해 특정 유저의 프로필 정보 조회
    - `postList`에 해당 프로필의 게시글 리스트 추가

--- FE/lib/controllers/profile_controller.dart
+++ FE/lib/controllers/profile_controller.dart
@@ -1,6 +1,55 @@
 import 'package:get/get.dart';
 
+import '../services/api_service.dart';
+import '../services/token_service.dart';
+
+import '../models/profile_model.dart';
+
 class ProfileController extends GetxController {
-  var count = 0.obs;
-  void increment() => count++;
+  final ApiService apiService;
+  final TokenService tokenService;
+
+  ProfileController({required this.apiService, required this.tokenService});
+
+  final profile = Profile(
+    message: '',
+    userId: 0,
+    nickname: '',
+    img: '',
+    likes: 0,
+    subscribes: 0,
+    postList: [],
+  ).obs;
+  final postList = <ProfilePostList>[].obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    Future.microtask(() {
+      _fetchMyProfile();
+    });
+  }
+
+  Future<void> _fetchMyProfile() async {
+    try {
+      String userId = await tokenService.loadUserId();
+      var response = await apiService.postRequest('ProfileInfo', {'target_user_id': userId, 'user_id': userId, 'post_count':10});
+      final Profile fetchedProfile = Profile.fromJson(response);
+      profile.value = fetchedProfile;
+      postList.value = fetchedProfile.postList;
+    } catch (e) {
+      print('Error fetching profile: $e');
+    }
+  }
+
+  Future<void> fetchProfile(int targetUserId) async {
+    try {
+      String userId = await tokenService.loadUserId();
+      var response = await apiService.postRequest('ProfileInfo', {'target_user_id': targetUserId, 'user_id': userId, 'post_count':10});
+      final Profile fetchedProfile = Profile.fromJson(response);
+      profile.value = fetchedProfile;
+    } catch (e) {
+      print('Error fetching profile: $e');
+    }
+  }
 }
