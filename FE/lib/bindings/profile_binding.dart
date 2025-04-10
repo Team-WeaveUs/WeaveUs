8f397e334fce3909e3c8d677610e912b021e7880
heauchi <68429278+tesupark@users.noreply.github.com>
2025-04-10 17:09:32 +0900

    feat: ProfileController에 ApiService, TokenService 주입

--- FE/lib/bindings/profile_binding.dart
+++ FE/lib/bindings/profile_binding.dart
@@ -1,9 +1,17 @@
 import 'package:get/get.dart';
 import '../controllers/profile_controller.dart';
+import '../services/api_service.dart';
+import '../services/token_service.dart';
 
 class ProfileBinding extends Bindings {
   @override
   void dependencies() {
-    Get.lazyPut<ProfileController>(() => ProfileController());
+    Get.lazyPut<TokenService>(() => TokenService());
+    Get.lazyPut<ApiService>(() => ApiService());
+
+    Get.lazyPut<ProfileController>(() => ProfileController(
+      apiService: Get.find(),
+      tokenService: Get.find(),
+    ));
   }
 }
