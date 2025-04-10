5588f47c63131cd0d4d141f32b74d04a0ab82b04
heauchi <68429278+tesupark@users.noreply.github.com>
2025-04-10 17:12:23 +0900

    feat: 게시물 상세 정보 조회 기능 구현
    - `post_controller.dart`: 게시물 상세 정보 조회 기능을 구현했습니다.
    - `post_binding.dart`: PostController에 대한 의존성 주입 설정을 추가했습니다.
    - `post_view.dart`: PostController를 사용하는 GetPage를 생성했습니다.

---
+++ FE/lib/bindings/post_binding.dart
@@ -0,0 +1,20 @@
+import 'package:get/get.dart';
+
+import '../services/api_service.dart';
+import '../services/token_service.dart';
+
+import '../controllers/post_controller.dart';
+
+class PostBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut<ApiService>(() => ApiService());
+    Get.lazyPut<TokenService>(() => TokenService());
+    Get.lazyPut<PostController>(() => PostController(
+      apiService: Get.find(),
+      tokenService: Get.find(),
+    ));
+
+  }
+
+}
---
+++ FE/lib/views/post_view.dart
@@ -0,0 +1,9 @@
+import 'package:get/get.dart';
+
+import '../controllers/post_controller.dart';
+
+class PostView extends GetPage<PostController> {
+  PostView({required super.name, required super.page});
+
+
+}
