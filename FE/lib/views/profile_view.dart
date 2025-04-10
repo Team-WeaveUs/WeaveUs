8007701be57fbd1deec3a61de5934294b6b542de
heauchi <68429278+tesupark@users.noreply.github.com>
2025-04-10 17:11:04 +0900

    feat: 프로필 화면 UI 구현 및 데이터 연동
    - 프로필 컨트롤러를 GetX GetView를 사용하여 연결
    - 프로필 데이터와 게시글 데이터를 화면에 표시
    - 데이터 로딩 중에는 로딩 인디케이터 표시
    - 게시글 목록을 GridView를 사용하여 이미지 표시

--- FE/lib/views/profile_view.dart
+++ FE/lib/views/profile_view.dart
@@ -1,16 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:weave_us/controllers/profile_controller.dart';
 
 import 'components/app_nav_bar.dart';
 import 'components/bottom_nav_bar.dart';
 
-class ProfileView extends StatelessWidget {
+class ProfileView extends GetView<ProfileController> {
   const ProfileView({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppNavBar(title: '내 프로필'),
-      body: const Placeholder(),
+      body: Column(children: [
+        Expanded(child: Obx(() {
+          if (controller.profile.value.nickname == '') {
+            return const Center(child: CircularProgressIndicator());
+          }
+          return Column(children: [
+            Text(controller.profile.value.nickname),
+            Text(controller.postList.length.toString()),
+            Image.network(controller.postList[0].img),
+            Expanded(child: GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3, childAspectRatio: 1),
+              itemCount: controller.postList.length,
+              itemBuilder: (context, index) {
+                final post = controller.postList[index];
+                return Image.network(post.img);
+              },
+            ))
+          ]);
+        }))
+      ]),
       bottomNavigationBar: BottomNavigation(),
     );
   }
