fd72d4f32e5afa5913613426185d5830f35af654
heauchi <68429278+tesupark@users.noreply.github.com>
2025-04-17 13:33:37 +0900

    feat: 탭 뷰 컨트롤러 및 프로필 바인딩 업데이트
    - `TabViewController`를 추가하여 탭 뷰를 관리하고, 탭 전환에 따른 뷰 구성을 동적으로 처리하도록 개선
    - `profile_binding.dart`에서 `ToggleController`에 대한 의존성을 제거
    - 탭 전환 시 렌더링 여부를 제어하기 위한 `isTabReady` 변수 추가
    - 탭 뷰에 대한 `MyPostWidget`, `ISubscribeWidget`, `MySubscribeWidget` 위젯 구성 추가
    - 토글 상태에 따라 탭 메뉴와 탭 뷰를 동적으로 구성하도록 `_setupTabs` 메서드 추가

--- FE/lib/bindings/profile_binding.dart
+++ FE/lib/bindings/profile_binding.dart
@@ -1,7 +1,6 @@
 import 'package:get/get.dart';
 import 'package:weave_us/controllers/tab_view_controller.dart';
 import '../controllers/profile_controller.dart';
-import '../controllers/toggle_controller.dart';
 import '../services/api_service.dart';
 import '../services/token_service.dart';
 
@@ -10,7 +9,6 @@ class ProfileBinding extends Bindings {
   void dependencies() {
     Get.lazyPut<TokenService>(() => TokenService());
     Get.lazyPut<ApiService>(() => ApiService());
-    Get.lazyPut<ToggleController>(() => ToggleController());
     Get.lazyPut<TabViewController>(() => TabViewController(
     ));
 
