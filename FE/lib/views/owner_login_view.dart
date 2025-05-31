cc134e525c81f89b27ed0a84fa022969a60eea28
Eunsseob <163013769+Eunsseob@users.noreply.github.com>
2025-06-01 00:03:42 +0900

    fix: 피그마 형식대로 UI 변경 <owner_login_view>

--- FE/lib/views/owner_login_view.dart
+++ FE/lib/views/owner_login_view.dart
@@ -12,6 +12,7 @@ class OwnerLoginView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       // appBar: AppBar(
       //   title: Text("OwnerLogin"),
       //   actions: [
